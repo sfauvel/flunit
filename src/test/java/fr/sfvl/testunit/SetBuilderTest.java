15ac3845a995c4c25191e7e1e5dae1fd0d208146
Sebastien Fauvel <sfauvel@gmail.com>
2012-04-28 00:24:47 +0200

    Complement des tests sur les builder de list

--- src/test/java/fr/sfvl/testunit/SetBuilderTest.java
+++ src/test/java/fr/sfvl/testunit/SetBuilderTest.java
@@ -8,12 +8,26 @@ import org.junit.Test;
 
 public class SetBuilderTest extends TestCase {
 	@Test
-	public void testSetBuilder() {
+	public void testBuild() {
 		Set<String> liste = SetBuilder.build("ValeurA", "ValeurB", "ValeurC");
 		
 		assertTrue(liste.contains("ValeurA"));
 		assertTrue(liste.contains("ValeurB"));
 		assertTrue(liste.contains("ValeurC"));
 		assertFalse(liste.contains("ValeurX"));
+	}
+	
+	/**
+	 * Vérification de la construction de la liste avec le bon type.
+	 */
+	@Test
+	public void testBuildVide() {
+		Set<String> listeString = SetBuilder.build();
+		assertTrue(listeString.isEmpty());
+		listeString.add("Valeur");
+		
+		Set<Integer> listeInt = SetBuilder.build();
+		assertTrue(listeInt.isEmpty());
+		listeInt.add(5);
 	}
 }
